a065f80a280a01f8071c8da5b43c69290087a7dc
Joey Storer <joeystorer@gmail.com>
2022-09-14 16:35:53 -0400

    adding main nav and updating config

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,13 @@
 <template>
-  <fragment>
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </fragment>
+  <MainNav />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import MainNav from "@/components/MainNav.vue";
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    MainNav: MainNav,
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
